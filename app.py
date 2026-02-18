521392a76e92585b51254a7332980dda50a65f22
jalenjaloney <jalendeloney@gmail.com>
2026-02-17 23:46:20 -0800

    styling tweaks

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 # flask_web/app.py
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
-
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=1079)
